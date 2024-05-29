81597d14175d474518ae0186c7bac583cbfef78f
yue666333 <1974915403@qq.com>
2024-05-29 20:06:18 +0800

    wms-web

---
+++ wms-web/scr/main/java/com/ken/wms/exception/StorageManageServiceException.java
@@ -0,0 +1,23 @@
+package com.ken.wms.exception;
+
+/**
+ * StorageManageServiceException异常
+ *
+ * 
+
+ */
+public class StorageManageServiceException extends BusinessException {
+
+    StorageManageServiceException(){
+        super();
+    }
+
+    public StorageManageServiceException(Exception e){
+        super(e);
+    }
+
+    StorageManageServiceException(Exception e, String exceptionDesc){
+        super(e, exceptionDesc);
+    }
+
+}
